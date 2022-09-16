e04b35c186fbf9c7cb0d003b214d6f6a42157a8c
Alexandru Nistor <a.nistor@student.tudelft.nl>
2022-09-16 19:15:55 +0200

    Implement a method that returns all values in a given Binary Search Tree in descending order.

---
+++ src/Exercises/BSTDescendingOrder.java
@@ -0,0 +1,45 @@
+
+
+import Exercises.Library.BinaryTree;
+
+import java.util.*;
+
+class BSTDescendingOrder {
+
+    /**
+     * Return all elements in the given BST in descending order.
+     *
+     * @param tree
+     *     The BST to traverse.
+     * @return A list of all elements in reverse order.
+     */
+    public static List<Integer> descendingOrder(BinaryTree tree) {
+        // TODO
+        Stack<BinaryTree> temp = new Stack();
+        temp.push(tree);
+        List<Integer> ans = new ArrayList();
+        while(!temp.isEmpty())
+        {    BinaryTree current = temp.peek();
+            if(current.hasRight())
+            {
+                temp.push(current.getRight());
+                current.setRight(null);
+            }
+            else if(current.hasLeft())
+            {
+                temp.pop();
+                temp.push(current.getLeft());
+                ans.add(current.getKey());
+            }
+            else{
+                temp.pop();
+                ans.add(current.getKey());
+            }
+        }
+        return ans;
+
+
+
+    }
+}
+
